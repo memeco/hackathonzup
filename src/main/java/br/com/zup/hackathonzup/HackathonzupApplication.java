a22fd844177f09a540681de324e581e8672b6331
Emerson Marques Pedro <emersonmp@gmail.com>
2024-09-12 16:02:40 -0300

    Adicionando novos arquivos

---
+++ src/main/java/br/com/zup/hackathonzup/HackathonzupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.hackathonzup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonzupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonzupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/hackathonzup/HackathonzupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.hackathonzup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonzupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
